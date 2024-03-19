1d9d86944048910a98b84c634c8d7d788f768bd7
HiddenLIV <jeongho9496@gmail.com>
2024-03-19 23:48:37 +0900

    webRTC 2일차 서버-웹 통신 이해

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,4 +1,58 @@
+// 프론트 엔드
 // alert("hi");
-console.log("hi");
+// console.log("hi");
 // const socket = new WebSocket("http://localhost:3000");
+
+const messageList = document.querySelector("ul");
+// const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", ()=>{
+    console.log("Connected to Server");
+})
+
+socket.addEventListener("message", (message)=>{
+    // 서버에서 socket.send로 보낸 메시지 받음
+    // console.log("Just got this:",message.data,"from the Server");
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
+})
+
+socket.addEventListener("close", ()=>{
+    console.log("Disconnected from Server");
+})
+
+// setTimeout(()=>{
+//     socket.send("hello from browser")
+// }, 5000)
+
+function makeMessage(type, payload){
+    const data ={type, payload}
+    return JSON.stringify(data);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    // socket.send(input.value);
+    socket.send(makeMessage("new_message", input.value))
+    input.value = "";
+}
+
+function handleNickSubmit(event){
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    // socket.send(input.value);
+    // socket.send({
+    //     type: "nickname",
+    //     payload: input.value
+    // });
+    socket.send(makeMessage("nickname", input.value));
+    input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit)
+nickForm.addEventListener("submit", handleNickSubmit)
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+// 서버
 import express from "express";
 import http from "http";
 import WebSocket from "ws";
@@ -19,10 +20,36 @@ const handleListen = () => {
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
 
-function handleConnecion(socket){
-    console.log(socket);
-}
+const sockets = [];
 
-wss.on("connection", handleConnecion);
+// function handleConnecion(socket){
+//     console.log(socket);
+// }
+
+// wss.on("connection", handleConnecion);
+
+wss.on("connection", (socket) => { //익명함수 처리
+    sockets.push(socket)
+    socket["nickname"] = "Anonymous";
+    console.log("Connected to Browser");
+    socket.on("close",()=>{console.log("Disconnected from Browser")});
+    socket.on("message", (msg)=>{
+        // 프론트에서 socket.send 로 보낸 데이터 받음
+        // console.log(`${message}`)
+        // socket.send(`${message}`);
+        const message = JSON.parse(msg);
+        // console.log(message.type, message.payload)
+        // sockets.forEach(aSocket => aSocket.send(`${message}`));
+        switch(message.type){
+            case "new_message":
+                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}:${message.payload}`));
+                break;
+            case "nickname":
+                socket["nickname"] = message.payload;
+                break;
+        }
+    })
+    // socket.send("Hello!")
+})
 
 server.listen(3000, handleListen);
